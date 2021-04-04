2c3a64d7717747494b830507aa941f890386e4b8
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-04-05 02:47:34 +0300

    New Exception Type Added

---
+++ Core/Extensions/AuthException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Extensions
+{
+    public class AuthException : Exception
+    {
+        public AuthException()
+        {
+            
+        }
+        public AuthException(string message) : base(message)
+        {
+
+        }
+        public AuthException(string message,Exception innerException) : base(message,innerException)
+        {
+
+        }
+    }
+}
