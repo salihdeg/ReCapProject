90ef7d38b7bf3b8f0ea00715862affe2051b72bb
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-04-05 02:52:17 +0300

    Fake Payment System Created

---
+++ Business/ValidationRules/FluentValidation/PaymentValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class PaymentValidator : AbstractValidator<PaymentInfo>
+    {
+        public PaymentValidator()
+        {
+            RuleFor(p => p.CardNumber).NotEmpty();
+            RuleFor(p => p.CardType).NotEmpty();
+            RuleFor(p => p.CVC).NotEmpty();
+            RuleFor(p => p.ExpiryDate).NotEmpty();
+            RuleFor(p => p.CVC).MaximumLength(3);
+            RuleFor(p => p.CardNumber).MaximumLength(16);
+        }
+    }
+}
