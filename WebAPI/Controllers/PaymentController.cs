e22a179b49a503e50c9ddbf50cec253b63eb737a
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-04-05 02:51:06 +0300

    Fake Payment System Created

---
+++ WebAPI/Controllers/PaymentController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        IPaymentService _paymentService;
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [HttpPost("getpayment")]
+        public IActionResult GetPayment(PaymentQuery paymentQuery)
+        {
+            PaymentInfo paymentInfo = paymentQuery.paymentInfo;
+            Rental rental = paymentQuery.rental;
+            var result = _paymentService.GetPayment(paymentInfo, rental);
+            return Ok(result);
+        }
+    }
+}
